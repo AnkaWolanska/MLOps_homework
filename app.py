b35f9b45f5a30131976b478ca1b2b776d8e55c6d
Anka Wolańska <162566937+AnkaWolanska@users.noreply.github.com>
2025-04-22 22:46:22 +0200

    removed the big file

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import cloudpickle
+from pathlib import Path
 
 app = FastAPI()
 
@@ -12,12 +14,19 @@ class PredictResponse(BaseModel):
     prediction: str
 
 
+ARTIFACTS_DIR = Path("app/artifacts")
+MODEL_PATH = ARTIFACTS_DIR / "inference_class.pkl"
+
+with open(MODEL_PATH, "rb") as file:
+    Inference = cloudpickle.load(file)
+
+print(Inference.__doc__)
+
+inference = Inference(...)
+
+
 @app.post("/predict", response_model=PredictResponse)
 async def predict(request: PredictRequest):
     input_text = request.text
-    if "great" in input_text.lower():
-        prediction = "positive"
-    else:
-        prediction = "negative"
-
+    prediction = inference.predict(input_text)
     return PredictResponse(prediction=prediction)
